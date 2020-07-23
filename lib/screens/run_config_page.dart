94160a5e14dffd11ddf3e12e8a3854b10d7d2770
pr4aveen <ajjuu98@gmail.com>
2020-07-23 22:53:39 +0800

    Implemented FutureBuilder in run config page

--- lib/screens/run_config_page.dart
+++ lib/screens/run_config_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:mapbox_gl/mapbox_gl.dart';
+import 'package:geolocator/geolocator.dart';
 
 import './run_progress_page.dart';
 import '../services/route_drawer.dart';
@@ -15,20 +16,36 @@ class RunConfigPage extends StatefulWidget {
 
 class _RunConfigPageState extends State<RunConfigPage> {
   MapboxMapController mapController;
-  double generateDistance = 3.0;
-  LatLng start = LatLng(1.2839, 103.8607);
-  bool generated = false;
-  double actualDistance = 0.0;
+  double _generateDistance = 3.0;
+  Future<LatLng> _start;
+  bool _generated = false;
+  double _actualDistance = 0.0;
+  bool _isGenerating = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _start = _setCurrentLocation();
+  }
+
+  Future<LatLng> _setCurrentLocation() async {
+    Position pos = await Geolocator().getCurrentPosition();
+    return LatLng(pos.latitude, pos.longitude);
+  }
 
   void _addRoute() async {
-    RouteGenerator routeGenerator =
-        RouteGenerator(start: start, generateDistance: generateDistance);
+    setState(() {
+      _isGenerating = true;
+    });
+    RouteGenerator routeGenerator = RouteGenerator(
+        start: await _start, generateDistance: _generateDistance);
 
     List<LatLng> route = await routeGenerator.generateRoute(0.1);
-    generated = true;
+    _generated = true;
     RouteDrawer.drawRoute(controller: mapController, route: route);
     setState(() {
-      actualDistance = routeGenerator.actualDistance;
+      _actualDistance = routeGenerator.actualDistance;
+      _isGenerating = false;
     });
   }
 
@@ -40,102 +57,121 @@ class _RunConfigPageState extends State<RunConfigPage> {
           'Generate Run Route',
         ),
       ),
-      body: Column(
-        children: [
-          Stack(
-            children: <Widget>[
-              Container(
-                child: MapboxMap(
-                  onMapCreated: (controller) => mapController = controller,
-                  initialCameraPosition:
-                      CameraPosition(target: start, zoom: 14),
-                  rotateGesturesEnabled: false,
-                  tiltGesturesEnabled: false,
-                ),
-                width: MediaQuery.of(context).size.width,
-                height: MediaQuery.of(context).size.height * 0.60,
-              ),
-              Positioned.fill(
-                child: Align(
-                  alignment: Alignment.bottomRight,
-                  child: Container(
-                    margin: EdgeInsets.only(bottom: 5.0, right: 5.0),
-                    decoration: BoxDecoration(
-                        color: Theme.of(context).accentColor,
-                        borderRadius: BorderRadius.all(Radius.circular(20))),
-                    padding: EdgeInsets.all(8),
-                    child: Text(
-                      'Route distance: ${actualDistance.toStringAsFixed(1)} km',
-                      style: TextStyle(
-                        fontSize: 16,
+      body: FutureBuilder(
+        future: _start,
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) {
+            return Center(child: CircularProgressIndicator());
+          } else {
+            return Column(
+              children: [
+                _isGenerating
+                    ? Container(
+                        alignment: Alignment.center,
+                        child: CircularProgressIndicator(),
+                        width: MediaQuery.of(context).size.width,
+                        height: MediaQuery.of(context).size.height * 0.60,
+                      )
+                    : Stack(
+                        children: <Widget>[
+                          Container(
+                            child: MapboxMap(
+                              onMapCreated: (controller) =>
+                                  mapController = controller,
+                              initialCameraPosition: CameraPosition(
+                                  target: snapshot.data, zoom: 14),
+                              rotateGesturesEnabled: false,
+                              tiltGesturesEnabled: false,
+                            ),
+                            width: MediaQuery.of(context).size.width,
+                            height: MediaQuery.of(context).size.height * 0.60,
+                          ),
+                          Positioned.fill(
+                            child: Align(
+                              alignment: Alignment.bottomRight,
+                              child: Container(
+                                margin:
+                                    EdgeInsets.only(bottom: 5.0, right: 5.0),
+                                decoration: BoxDecoration(
+                                    color: Theme.of(context).accentColor,
+                                    borderRadius:
+                                        BorderRadius.all(Radius.circular(20))),
+                                padding: EdgeInsets.all(8),
+                                child: Text(
+                                  'Route distance: ${_actualDistance.toStringAsFixed(1)} km',
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          )
+                        ],
                       ),
+                SizedBox(
+                  height: 10,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    PlaceholderWidget(
+                      title: 'Target Distance',
+                      message: '${_generateDistance.toStringAsFixed(1)} km',
+                      reversed: true,
                     ),
+                    PlaceholderWidget(
+                      title: 'Actual Distance',
+                      message: '${_actualDistance.toStringAsFixed(1)} km',
+                      reversed: true,
+                    ),
+                  ],
+                ),
+                SliderTheme(
+                  data: SliderTheme.of(context).copyWith(
+                    thumbShape: RoundSliderThumbShape(
+                      enabledThumbRadius: 15.0,
+                    ),
+                    overlayShape: RoundSliderOverlayShape(
+                      overlayRadius: 30.0,
+                    ),
+                    activeTrackColor: Colors.white,
+                    thumbColor: Theme.of(context).primaryColor,
+                    overlayColor: Color(0x29EB1555),
+                    inactiveTrackColor: Theme.of(context).accentColor,
+                  ),
+                  child: Slider(
+                    min: 3,
+                    max: 30,
+                    value: _generateDistance.toDouble(),
+                    onChanged: (double dist) {
+                      setState(() {
+                        _generateDistance = dist;
+                      });
+                    },
                   ),
                 ),
-              )
-            ],
-          ),
-          SizedBox(
-            height: 10,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: <Widget>[
-              PlaceholderWidget(
-                title: 'Target Distance',
-                message: '${generateDistance.toStringAsFixed(1)} km',
-                reversed: true,
-              ),
-              PlaceholderWidget(
-                title: 'Actual Distance',
-                message: '${actualDistance.toStringAsFixed(1)} km',
-                reversed: true,
-              ),
-            ],
-          ),
-          SliderTheme(
-            data: SliderTheme.of(context).copyWith(
-              thumbShape: RoundSliderThumbShape(
-                enabledThumbRadius: 15.0,
-              ),
-              overlayShape: RoundSliderOverlayShape(
-                overlayRadius: 30.0,
-              ),
-              activeTrackColor: Colors.white,
-              thumbColor: Theme.of(context).primaryColor,
-              overlayColor: Color(0x29EB1555),
-              inactiveTrackColor: Theme.of(context).accentColor,
-            ),
-            child: Slider(
-              min: 3,
-              max: 30,
-              value: generateDistance.toDouble(),
-              onChanged: (double dist) {
-                setState(() {
-                  generateDistance = dist;
-                });
-              },
-            ),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: <Widget>[
-              RaisedButton(
-                child: Text('Generate'),
-                onPressed: _addRoute,
-                color: Theme.of(context).primaryColor,
-              ),
-              RaisedButton(
-                child: Text('Run'),
-                onPressed: generated
-                    ? () => Navigator.pushNamedAndRemoveUntil(
-                        context, RunProgressPage.routeName, (_) => false)
-                    : null,
-                color: Theme.of(context).primaryColor,
-              ),
-            ],
-          )
-        ],
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    RaisedButton(
+                      child: Text('Generate'),
+                      onPressed: _addRoute,
+                      color: Theme.of(context).primaryColor,
+                    ),
+                    RaisedButton(
+                      child: Text('Run'),
+                      onPressed: _generated
+                          ? () => Navigator.pushNamedAndRemoveUntil(
+                              context, RunProgressPage.routeName, (_) => false)
+                          : null,
+                      color: Theme.of(context).primaryColor,
+                    ),
+                  ],
+                )
+              ],
+            );
+          }
+        },
       ),
     );
   }
