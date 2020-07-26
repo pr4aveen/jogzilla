0d5681e1fb424547c009908a73b441607bec7b56
xinyue <xinyue98.zhang@gmail.com>
2020-07-26 21:11:15 +0800

    Fixed calories calculation

--- lib/services/calories_calculator.dart
+++ lib/services/calories_calculator.dart
@@ -10,9 +10,5 @@ class CaloriesCalculator {
     return ratio * weight * distance;
   }
 
-  // calories per min = 0.0175 * met * speed(mph) * weight
-  // cal per min = 0.0175 * met * 1.60934 km per hr * weight
-  // cal = 0.0175 * 1.6 * 1.60934 * km / 60 * weight
-
   // Calories burned per minute = (MET x body weight in Kg x 3.5) ÷ 200
 }
