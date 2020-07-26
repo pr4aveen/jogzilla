64eccfb531d474c556d9a32333b7924340933c94
xinyue <xinyue98.zhang@gmail.com>
2020-07-26 21:08:14 +0800

    Fixed calories calculation

--- lib/services/calories_calculator.dart
+++ lib/services/calories_calculator.dart
@@ -1,6 +1,6 @@
 class CaloriesCalculator {
   static final averageWeight = 62.0;
-  static final ratio = 1.6 * 0.0175 * 1.60934 / 60;
+  static final ratio = 1.6 * 0.0175 / 1.60934 * 60;
   double weight;
 
   CaloriesCalculator({this.weight});
@@ -11,5 +11,8 @@ class CaloriesCalculator {
   }
 
   // calories per min = 0.0175 * met * speed(mph) * weight
+  // cal per min = 0.0175 * met * 1.60934 km per hr * weight
   // cal = 0.0175 * 1.6 * 1.60934 * km / 60 * weight
+
+  // Calories burned per minute = (MET x body weight in Kg x 3.5) ÷ 200
 }
